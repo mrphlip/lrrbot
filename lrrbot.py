7916273665a9d4554924cddcbb46210703b6e896
Laurent Dufresne <cyberxxl5@gmail.com>
2014-02-04 21:23:34 -0500

    !drink
    added the much requested drinking game link

--- lrrbot.py
+++ lrrbot.py
@@ -171,6 +171,10 @@ class LRRBot(irc.bot.SingleServerIRCBot):
 	def on_command_fixtable(self, conn, event, params, respond_to):
 		conn.privmsg(respond_to, "┳━┳ ノ(º_ºノ)")
 
+        @utils.throttle(5)
+	def on_command_drink(self, conn, event, params, respond_to):
+		conn.privmsg(respond_to, "The drinking game is: http://bit.ly/YRRLRLager)
+
 	@utils.throttle(5)
 	def on_command_powah(self, conn, event, params, respond_to):
 		conn.privmsg(respond_to, "ᕦ(° Д°)ᕤ STOPPIN POWAH")
