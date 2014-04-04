24778be0fb7f17f13ed89cd2610d29723e88d583
phlip <github@mrphlip.com>
2014-04-04 22:39:28 +1100

    Fix up how the server is started

---
+++ www/master.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import server
+import index
+import notifications
+import stats
+import login
+import archive
+import secrets
+
+server.app.secret_key = secrets.session_secret
+
+app = server.app
+__all__ = ['app']
--- www/testserver.py
+++ www/testserver.py
@@ -1,11 +1,3 @@
 #!/usr/bin/env python3
-import server
-import index
-import notifications
-import stats
-import login
-import archive
-import secrets
-
-server.app.secret_key = secrets.session_secret
-server.app.run(debug=True, threaded=True)
+from master import app
+app.run(debug=True, threaded=True)
