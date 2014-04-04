2a1c4f5ca786ff6ee8c4bb46ba2c7799eeb7cd28
phlip <github@mrphlip.com>
2014-04-04 21:38:42 +1100

    More safety checks

--- lrrbot.py
+++ lrrbot.py
@@ -237,7 +237,7 @@ class LRRBot(irc.bot.SingleServerIRCBot):
 
 	@utils.throttle(60)
 	def subcommand_game_vote_respond(self, conn, event, respond_to, game):
-		if game.get('votes'):
+		if game and game.get('votes'):
 			good = sum(game["votes"].values())
 			count = len(game["votes"])
 			conn.privmsg(respond_to, "Rating for %s is now %.0f%% (%d/%d)" % (self.game_name(game), 100*good/count, good, count))
