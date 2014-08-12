3819a88432e581a8f287e20e821b64b364ecd26d
Andreas Ots <andreasots@gmail.com>
2014-08-12 13:42:20 +0300

    remove !#d#

--- commands/misc.py
+++ commands/misc.py
@@ -6,7 +6,6 @@ import utils
 import storage
 import twitch
 import json
-import random
 
 @bot.command("test")
 @utils.mod_only
@@ -152,89 +151,3 @@ def viewers(lrrbot, conn, event, respond_to):
 	else:
 		chatters = "No-one in the chat."
 	conn.privmsg(respond_to, "%s %s" % (viewers, chatters))
-
-DICE_NAMES = {
-	1: ("a marble", "marbles"),
-	2: ("a coin", "coins"),
-	4: ("a tetrahedron", "tetrahedra"),
-	6: ("a cube", "cubes"),
-	8: ("an octahedron", "octahedra"),
-	10: ("a pentagonal trapezohedron", "pentagonal trapezohedra"),
-	12: ("a dodecahedron", "dodecahedra"),
-	20: ("an icosahedron", "icosahedra"),
-	100: ("a pair of pentagonal trapezohedra", "pairs of pentagonal trapezohedra"),
-}
-INSULTS = [
-	"Hmm",
-	"Huh",
-	"Duh",
-	"Funny how that works",
-	"Surprising everyone"
-]
-@bot.command("(\d*)d(\d+)")
-@utils.throttle(5)
-@utils.mod_only
-def dice(lrrbot, conn, event, respond_to, count, sides):
-	"""
-	Command: !d#
-	Command: !#d#
-
-	Roll one or more dice. For example: !d20 or !2d6
-	"""
-	if count == "":
-		count = 1
-	else:
-		count = int(count)
-	sides = int(sides)
-	plural = (count != 1)
-
-	if count <= 0:
-		conn.privmsg(respond_to, "Rolling no things... got nothing. %s." % random.choice(INSULTS))
-		return
-	elif count > 100:
-		if sides in DICE_NAMES:
-			name = DICE_NAMES[sides][1]
-		else:
-			name = "d%ds" % sides
-		conn.privmsg(respond_to, "I don't have that many %s..." % name)
-		return
-	elif sides <= 0:
-		conn.privmsg(respond_to, "I'm not getting that close to the black hole" + ("s" if plural else ""))
-		return
-	elif sides == 1:
-		if plural:
-			conn.privmsg(respond_to, "Rolling %d marbles... got %d. %s." % (count, count, random.choice(INSULTS)))
-		else:
-			conn.privmsg(respond_to, "Rolling a marble... got 1. %s." % random.choice(INSULTS))
-		return
-	elif sides >= 1000:
-		conn.privmsg(respond_to, "Are you sure you want THE EVENT to happen? [y/n]")
-		return
-
-	rolls = [random.randint(1, sides) for i in range(count)]
-	result = sum(rolls)
-	if sides == 2:
-		result -= count # as though we were rolling 0 and 1, instead of 1 and 2
-		if plural:
-			conn.privmsg(respond_to, "Flipping %d coins... got %d %s and %d %s." % (count, result, "heads" if result == 1 else "headses", count - result, "tails" if count - result == 1 else "tailses"))
-		else:
-			conn.privmsg(respond_to, "Flipping a coin... got %s." % ("Heads", "Tails")[result])
-	else:
-		if sides in DICE_NAMES:
-			if plural:
-				name = "%d %s" % (count, DICE_NAMES[sides][1])
-			else:
-				name = DICE_NAMES[sides][0]
-		else:
-			if plural:
-				name = "%dd%d" % (count, sides)
-			else:
-				name = "d%d" % sides
-		if plural and count <= 10 and sides <= 1000:
-			conn.privmsg(respond_to, "Rolling %s... got %d (%s)." % (name, result, ", ".join(str(i) for i in rolls)))
-		else:
-			conn.privmsg(respond_to, "Rolling %s... got %d." % (name, result))
-
-@bot.command("coin")
-def coin(lrrbot, conn, event, respond_to):
-	dice(lrrbot, conn, event, respond_to, 1, 2)
