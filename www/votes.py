4a70aaae0b1d28ce88ea092333e1652fc43e4d2b
Andreas Ots <andreasots@gmail.com>
2014-09-08 01:03:45 +0300

    Group stats by show (#33): fix /votes

--- www/votes.py
+++ www/votes.py
@@ -7,23 +7,29 @@ import botinteract
 @server.app.route('/votes')
 @login.require_login
 def votes(session):
-	data = botinteract.get_data('games')
+	data = botinteract.get_data('shows')
 	current_game_id = botinteract.get_current_game()
+	current_show_id = botinteract.get_show()
 
-	if current_game_id not in data:
+	if current_game_id not in data[current_show_id]["games"]:
 		current_game_id = None
-	games = [{
-		"id": game_id,
-		"name": game["name"],
-		"display": game.get("display", game["name"]),
-		"vote": game.get("votes", {}).get(session["user"]),
-	} for game_id,game in data.items()]
-	games.sort(key=lambda game:(1 if game['id'] == current_game_id else 2, game['display'].upper()))
+	shows = [{
+		"id": show_id,
+		"name": show.get("display", show_id),
+		"games": sorted([{
+				"id": game_id,
+				"name": game["name"],
+				"display": game.get("display", game["name"]),
+				"vote": game.get("votes", {}).get(session["user"]),
+			    } for game_id,game in show['games'].items()],
+			key=lambda game: (1 if game['id'] == current_game_id and show_id == current_show_id else 2, game['display'].upper()))
+	} for show_id, show in data.items()]
+	shows.sort(key=lambda show: (1 if show["id"] == current_show_id and current_game_id is not None else 2, show["name"].upper()))
 
-	return flask.render_template("votes.html", games=games, current_game_id=current_game_id, session=session)
+	return flask.render_template("votes.html", shows=shows, current_show_id=current_show_id, current_game_id=current_game_id, session=session)
 
 @server.app.route('/votes/submit', methods=['POST'])
 @login.require_login
 def vote_submit(session):
-	botinteract.set_data(['games', flask.request.values['id'], 'votes', session['user']], bool(int(flask.request.values['vote'])))
+	botinteract.set_data(['shows', flask.request.values['show'], 'games', flask.request.values['id'], 'votes', session['user']], bool(int(flask.request.values['vote'])))
 	return flask.json.jsonify(success='OK')
