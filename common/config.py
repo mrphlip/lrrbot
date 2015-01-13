364713b17977470f440a00a7ec3b385dd54ba3a5
Andreas Ots <andreasots@gmail.com>
2015-01-13 20:23:50 +0200

    pick "apipass" from apipass section

--- common/config.py
+++ common/config.py
@@ -10,8 +10,14 @@ CONFIG_SECTION = 'lrrbot'
 
 config = configparser.ConfigParser()
 config.read(argv.conf)
+
 mysqlopts = dict(config.items("mysqlopts"))
+
+if "use_unicode" in mysqlopts:
+	mysqlopts["use_unicode"] = mysqlopts["use_unicode"].lower() == "true"
+
 apipass = dict(config.items("apipass"))
+
 config = dict(config.items(CONFIG_SECTION))
 
 # hostname - server to connect to (default Twitch)
@@ -56,7 +62,10 @@ config.setdefault('commandprefix', '!')
 # siteurl - root of web site
 config.setdefault('siteurl', 'http://lrrbot.mrphlip.com/')
 # apipass - secret string needed to communicate with web site
-config.setdefault('apipass', None)
+config["apipass"] = None
+for secret, user in apipass.items():
+	if user == config["username"]:
+		config["apipass"] = secret
 
 # mods - comma-separated list of moderators for the bot, in addition to people with chanop privileges
 config['mods'] = set(i.strip().lower() for i in config.get('mods', 'd3fr0st5,mrphlip,lord_hosk,admiralmemo,dixonij').split(','))
