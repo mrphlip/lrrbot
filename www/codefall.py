15ff07c8b0983fb00f494e31f70ab151d376b47d
Andreas Ots <andreasots@gmail.com>
2015-05-04 15:36:20 +0300

    codefall: web side

--- www/codefall.py
+++ www/codefall.py
@@ -32,118 +32,99 @@ from www import login
 @utils.with_postgres
 def codefall(conn, cur, session):
 	"""Show a list of all codefall pages and a form to add new ones."""
-	session = request.environ.get("beaker.session")
-	user_name = session.get("user_name")
-
-	# we can't retrieve keys without user name
-	if not user_name:
-		return template("codefall", session=session, subtitle="Codefall")
 
 	# get all codes for the user from the database
-	codes_qry = """SELECT cid, description, code, code_type, claimed
-			FROM codefall
-			WHERE user_name = :user_name"""
-	codes = db.execute(codes_qry, {"user_name": user_name})
-
+	cur.execute("""
+		SELECT cid, description, code, code_type, claimed
+		FROM codefall
+		WHERE user_name = %s
+	""", (session["user"],))
+	print(session["user"])
 	unclaimed, claimed = list(), list()
-	for code in codes:
-		entry = {"description": code.description, "code_type": code.code_type}
+	for cid, description, code, code_type, is_claimed in cur:
+		entry = {"description": description, "code_type": code_type}
 
-		if not code.claimed:
+		if not is_claimed:
 			# for unclaimed codes we need to generate our "random" link
-			secret = int_to_codefall_key(code.cid)
-			secret_url = CODEFALL_SHOW_URL.format(secret=secret)
-			entry["secret_url"] = secret_url
+			cid = utils.hmac_sign(str(cid).encode("utf-8")).decode("utf-8")
+			entry["url"] = flask.url_for("codefall_show", code=cid, _external=True)
 			unclaimed.append(entry)
 		else:
 			claimed.append(entry)
 
-	return template("codefall", session=session, subtitle="Codefall", unclaimed=unclaimed, claimed=claimed)
+	return flask.render_template("codefall.html", session=session, unclaimed=unclaimed, claimed=claimed)
 
-
-def handle_codefall_add(db):
-	"""Add a new codefall page."""
-	session = request.environ.get("beaker.session")
-	# we require users to be logged in when adding new codes
-	if not session.get("logged_in", False):
-		redirect("/codefall")
-
-	# get all mandatory items
-	user_name = session.get("user_name")
-	description = request.forms.getunicode("description")
-	code = request.forms.getunicode("code")
-	code_type = request.forms.getunicode("code_type")
-	if not all((user_name, description, code, code_type)):
-		redirect("/codefall")
-
-	new_code_qry = """
+@server.app.route("/codefall/add", methods=["POST"])
+@login.require_login
+@utils.with_postgres
+def codefall_add(conn, cur, session):
+	cur.execute("""
 		INSERT INTO codefall (description, code, code_type, user_name)
-                      VALUES (:description,:code, :code_type, :user_name)"""
-	try:
-		db.execute(new_code_qry,
-			{"description": description,
-			"code": code,
-			"code_type": code_type,
-			"user_name": user_name})
-	except IntegrityError:
-		redirect("/codefall")
+		VALUES (%s, %s, %s, %s)
+	""", (
+		flask.request.values["description"],
+		flask.request.values["code"],
+		flask.request.values["code_type"],
+		session["user"]
+	))
 
-	# everything seems fine, store the data now
-	db.commit()
+	return flask.redirect(flask.url_for("codefall"))
 
-	# now redirect back to codefall page
-	redirect("/codefall")
-
-def handle_codefall_claim(secret, db):
+@server.app.route("/codefall/claim/<code>")
+@login.require_login
+@utils.with_postgres
+def codefall_claim(conn, cur, session, code):
 	"""Claim a codefall page."""
-	session = request.environ.get("beaker.session")
 
 	# first, try to parse the secret
-	try:
-		cid = codefall_key_to_int(secret)
-	except:
-		return template("codefall_claim", session=session, subtitle="Codefall")
+	cid = utils.hmac_verify(code.encode("utf-8"))
+	if cid is None:
+		return utils.error("Invalid codefall code")
+	cid = int(cid)
 
-	claim_code_qry = """
+	cur.execute("""
 		UPDATE codefall
-		SET claimed = True
-		WHERE cid = :cid AND claimed = False
-		RETURNING description, code, code_type
-	"""
-
-	code = db.execute(claim_code_qry, {"cid": cid})
-	db.commit()
-	code = code.first()
-	if not code:
-		return template("codefall_claim", session=session, subtitle="Codefall")
-
-	entry = {"description": code.description, "code": code.code, "code_type": code.code_type}
-
-	return template("codefall_claim", session=session, subtitle="Codefall", entry=entry)
-
-def handle_codefall_show(secret, db):
-	"""Show a codefall page (letting people claim it)."""
-	session = request.environ.get("beaker.session")
-
-	# first, try to parse the secret
-	try:
-		cid = codefall_key_to_int(secret)
-	except:
-		return template("codefall_show", session=session, subtitle="Codefall")
-
-	show_code_qry = """
-		SELECT description, code_type
+		SET claimed = TRUE
+		WHERE cid = %s AND NOT claimed
+	""", (cid, ))
+	claimed = cur.rowcount == 0
+	cur.execute("""
+		SELECT description, code, code_type
 		FROM codefall
-		WHERE cid = :cid AND claimed = False
-	"""
+		WHERE cid = %s
+	""", (cid, ))
 
-	code = db.execute(show_code_qry, {"cid": cid})
-	code = code.first()
-	if not code:
-		return template("codefall_show", session=session, subtitle="Codefall")
+	for description, code, code_type in cur:
+		return flask.render_template("codefall_claim.html",
+			session=session,
+			description=description,
+			code=code,
+			code_type=code_type,
+			claimed=claimed
+		)
 
-	claim_url = CODEFALL_CLAIM_URL.format(secret=secret)
+@server.app.route("/codefall/<code>")
+@login.require_login
+@utils.with_postgres
+def codefall_show(conn, cur, session, code):
+	"""Show a codefall page (letting people claim it)."""
+	# first, try to parse the secret
+	cid = utils.hmac_verify(code.encode("utf-8"))
+	if cid is None:
+		return utils.error_page("Invalid codefall code")
+	cid = int(cid)
 
-	entry = {"description": code.description, "claim_url": claim_url, "code_type": code.code_type}
+	cur.execute("""
+		SELECT description, code_type, claimed
+		FROM codefall
+		WHERE cid = %s AND NOT claimed
+	""", (cid, ))
 
-	return template("codefall_show", session=session, subtitle="Codefall", entry=entry)
+	for description, code_type, claimed in cur:
+		url = flask.url_for("codefall_claim", code=code)
+		return flask.render_template("codefall_show.html", session=session,
+			       description=description,
+			       code_type=code_type,
+			       url=url,
+			       claimed=claimed,
+		)
