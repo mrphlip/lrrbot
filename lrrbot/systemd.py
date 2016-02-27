ea12ca73ba8d52578b64c8da1af2761cd71d6c06
Andreas Ots <andreasots@users.noreply.github.com>
2016-02-27 10:18:46 +0200

    FFS

--- lrrbot/systemd.py
+++ lrrbot/systemd.py
@@ -4,29 +4,29 @@ import os
 log = logging.getLogger("lrrbot.systemd")
 
 try:
-    from systemd.daemon import notify
+	from systemd.daemon import notify
 except ImportError:
-    log.warning("`python-systemd` not installed")
+	log.warning("`python-systemd` not installed")
 
-    def notify(status, unset_environment=False, pid=0, fds=None):
-        return False
+	def notify(status, unset_environment=False, pid=0, fds=None):
+		return False
 
 class Service:
-    def __init__(self, loop):
-        self.loop = loop
+	def __init__(self, loop):
+		self.loop = loop
 
-        timeout_usec = os.environ.get("WATCHDOG_USEC")
-        if timeout_usec is not None:
-            self.timeout = (int(timeout_usec) * 1e-6) / 2
-            self.watchdog_handle = self.loop.call_later(self.timeout, self.watchdog)
+		timeout_usec = os.environ.get("WATCHDOG_USEC")
+		if timeout_usec is not None:
+			self.timeout = (int(timeout_usec) * 1e-6) / 2
+			self.watchdog_handle = self.loop.call_later(self.timeout, self.watchdog)
 
-        self.subsystems = {"irc", "whispers"}
+		self.subsystems = {"irc", "whispers"}
 
-    def watchdog(self):
-        notify("WATCHDOG=1")
-        self.watchdog_handle = self.loop.call_later(self.timeout, self.watchdog)
+	def watchdog(self):
+		notify("WATCHDOG=1")
+		self.watchdog_handle = self.loop.call_later(self.timeout, self.watchdog)
 
-    def subsystem_started(self, subsystem):
-        self.subsystems.remove(subsystem)
-        if self.subsystems == set():
-            notify("READY=1")
+	def subsystem_started(self, subsystem):
+		self.subsystems.remove(subsystem)
+		if self.subsystems == set():
+			notify("READY=1")
