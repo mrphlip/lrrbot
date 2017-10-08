adcff2474338f9f65fcc53ddb6576d36d61a6805
Andreas Ots <qrpth@qrpth.eu>
2017-10-08 09:00:43 +0300

    eris: add !time

--- eris/commands/__init__.py
+++ eris/commands/__init__.py
@@ -1,6 +1,8 @@
 import eris.commands.live
+import eris.commands.time
 import eris.commands.voice
 
 def register(command_parser):
-    eris.commands.live.register(command_parser)
-    eris.commands.voice.register(command_parser)
+	eris.commands.live.register(command_parser)
+	eris.commands.time.register(command_parser)
+	eris.commands.voice.register(command_parser)
