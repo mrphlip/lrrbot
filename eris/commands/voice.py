b28c9962289abd4194e81ec0737585850755a8fa
Andreas Ots <qrpth@qrpth.eu>
2017-10-24 19:36:22 +0300

    eris: create temporary voice channels in the voice channel category

--- eris/commands/voice.py
+++ eris/commands/voice.py
@@ -1,5 +1,6 @@
 import discord
 from common.config import config
+from common import http
 
 def register(bot):
 	@bot.command("voice (.*?)")
@@ -7,5 +8,15 @@ def register(bot):
 		if command.server is None:
 			await bot.eris.send_message(command.channel, "Can't create temporary voice channels over private messages.")
 			return
-		channel = await bot.eris.create_channel(command.server, config['discord_temp_channel_prefix'] + desc, type=discord.ChannelType.voice)
+
+		channel_name = config['discord_temp_channel_prefix'] + desc
+		headers = {
+			"Authorization": "Bot " + config['discord_botsecret'],
+		}
+		await http.request_coro("https://discordapp.com/api/v6/guilds/%s/channels" % command.server.id, method="POST", asjson=True, headers=headers, data={
+			"name": config['discord_temp_channel_prefix'] + desc,
+			"type": discord.ChannelType.voice.value,
+			"parent_id": config['discord_category_voice'],
+		})
+
 		await bot.eris.send_message(command.channel, "Created a temporary voice channel %r." % desc)
