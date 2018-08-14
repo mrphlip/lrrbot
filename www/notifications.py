0b15ef7c1c45c1a2013cda1d85b9e90db77dc208
phlip <github@mrphlip.com>
2018-08-14 23:39:52 +1000

    Support for multi-gifts

--- www/notifications.py
+++ www/notifications.py
@@ -13,6 +13,8 @@ from www import server
 from www import login
 
 MILESTONES = [
+	("Multi-Gift Subscriptions", config["timezone"].localize(datetime.datetime(2018, 8, 9, 9, 0))),
+	("Gift Subscriptions", config["timezone"].localize(datetime.datetime(2017, 11, 15, 9, 0))),
 	("Twitch Prime", config["timezone"].localize(datetime.datetime(2016, 9, 30, 12, 0))),
 	("LoadingReadyLive premiere", config["timezone"].localize(datetime.datetime(2016, 5, 14, 17, 0))),
 	("Pre-PreRelease premiere", config["timezone"].localize(datetime.datetime(2016, 3, 26, 12, 0))),
@@ -28,17 +30,18 @@ def get_events():
 	recent_events = []
 	query = sqlalchemy.select([events.c.id, events.c.event, events.c.data, events.c.time, sqlalchemy.func.current_timestamp() - events.c.time]) \
 		.where(events.c.time > sqlalchemy.func.current_timestamp() - datetime.timedelta(days=2)) \
-		.where(events.c.event.in_({'twitch-subscription', 'twitch-resubscription', 'twitch-message', 'twitch-cheer', 'patreon-pledge'})) \
+		.where(events.c.event.in_({'twitch-subscription', 'twitch-resubscription', 'twitch-subscription-mysterygift', 'twitch-message', 'twitch-cheer', 'patreon-pledge'})) \
 		.order_by(events.c.time.desc())
 	with server.db.engine.begin() as conn:
 		for id, event, data, time, duration in conn.execute(query):
-			data['time'] = time
-			recent_events.append({
-				'id': id,
-				'event': event,
-				'data': data,
-				'duration': common.time.nice_duration(duration, 2)
-			})
+			if not data.get('ismulti'):
+				data['time'] = time
+				recent_events.append({
+					'id': id,
+					'event': event,
+					'data': data,
+					'duration': common.time.nice_duration(duration, 2)
+				})
 		last_event_id = conn.execute(sqlalchemy.select([sqlalchemy.func.max(events.c.id)])).first()
 		last_event_id = last_event_id[0] if last_event_id is not None else 0
 	return last_event_id, recent_events
