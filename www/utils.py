6410bb59716f8a2d60f8f450b748d6b245e23d58
phlip <github@mrphlip.com>
2019-03-13 21:11:26 +1100

    Roll this into the existing filter

--- www/utils.py
+++ www/utils.py
@@ -8,12 +8,13 @@ import jinja2.utils
 
 from common import config
 from common.utils import cache
+from common.time import nice_duration
 from www import login
 
 def error_page(message):
 	return flask.render_template("error.html", message=message, session=login.load_session(include_url=False))
 
-def timestamp(ts):
+def timestamp(ts, cls='timestamp', tag='span'):
 	"""
 	Outputs a given time (either unix timestamp or datetime instance) as a human-readable time
 	and includes tags so that common.js will convert the time on page-load to the user's
@@ -24,7 +25,16 @@ def timestamp(ts):
 	elif ts.tzinfo is None:
 		ts = ts.replace(tzinfo=datetime.timezone.utc)
 	ts = ts.astimezone(config.config['timezone'])
-	return flask.Markup("<span class=\"timestamp\" data-timestamp=\"{}\">{:%A, %d %B, %Y %H:%M:%S %Z}</span>".format(ts.timestamp(), ts))
+	if cls == 'timestamp-duration':
+		text = nice_duration(datetime.datetime.now(config.config['timezone']) - ts, 2)
+	else:
+		text = ts.strftime("%A, %d %B, %Y %H:%M:%S %Z")
+	return flask.Markup("<{tag} class=\"{cls}\" data-timestamp=\"{timestamp}\">{text}</{tag}>".format(
+		text=text,
+		timestamp=ts.timestamp(),
+		tag=tag,
+		cls=cls,
+	))
 
 @cache(period=None, params=[0])
 def static_url(filename):
