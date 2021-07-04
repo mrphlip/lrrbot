bbbcebf9ab125b2f9f83d5e10817433b7c545bec
phlip <github@mrphlip.com>
2021-07-04 21:12:58 +1000

    Cards can have plusses in them now, wild

--- common/card.py
+++ common/card.py
@@ -6,7 +6,7 @@ CARD_GAME_MTG = 1
 CARD_GAME_KEYFORGE = 2
 CARD_GAME_PTCG = 3
 
-re_specialchars = re.compile(r"[ \-'\",:!?.()\u00ae&/\u2019\u201c\u201d\[\]~\u2026\u25b9]")
+re_specialchars = re.compile(r"[ \-+'\",:!?.()\u00ae&/\u2019\u201c\u201d\[\]~\u2026\u25b9]")
 LETTERS_MAP = {
 	'\u00e0': 'a',
 	'\u00e1': 'a',
