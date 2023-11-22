df32fa7b3e6bdfda4dc1d6dc37ebcc3ad36d88d0
Andreas Ots <qrpth@qrpth.eu>
2023-11-22 09:14:21 +0200

    lrrbot.moderator_actions: implement more message types

--- lrrbot/moderator_actions.py
+++ lrrbot/moderator_actions.py
@@ -1,6 +1,8 @@
 import asyncio
 import datetime
 import sqlalchemy
+import dateutil.parser
+import json
 
 from common import pubsub
 from common import utils
@@ -31,8 +33,26 @@ class ModeratorActions:
 			pubsub.signals.signal(topic).connect(self.on_message)
 
 	def on_message(self, sender, message):
-		log.info("Got message: %r", message['data'])
+		log.info("Got a %s message: %r", message['type'], message['data'])
 
+		if message['type'] == 'moderation_action':
+			text, attachments = self.on_moderation_action(message)
+		elif message['type'] == 'channel_terms_action':
+			text, attachments = self.on_channel_terms_action(message)
+		elif message['type'] in ('moderator_added', 'moderator_removed', 'vip_added'):
+			text, attachments = self.on_role_action(message)
+		elif message['type'] in ('approve_unban_request', 'deny_unban_request'):
+			text, attachments = self.on_unban_request_action(message)
+		else:
+			user = message['data'].get('created_by') or message['data'].get('created_by_login') or 'someone'
+			text = f"{slack.escape(user)} did a {slack.escape(message['type'])}."
+			attachments = [
+				{"text": slack.escape(json.dumps(message['data']))}
+			]
+
+		asyncio.ensure_future(slack.send_message(text, attachments=attachments), loop=self.loop).add_done_callback(utils.check_exception)
+
+	def on_moderation_action(self, message):
 		action = message['data']['moderation_action']
 		args = message['data']['args']
 		mod = message['data']['created_by']
@@ -53,13 +73,13 @@ class ModeratorActions:
 			length = time.nice_duration(int(args[1]), 0) if args[1] != '' else '???'
 			reason = args[2] if len(args) >= 3 else None
 			text = "%s was%s timed out for %s by %s." % (slack.escape(user), " also" if same_user else "", slack.escape(length), slack.escape(mod))
-			if reason is not None:
+			if reason:
 				text += " Reason: %s" % slack.escape(reason)
 		elif action == 'ban':
 			user = args[0]
 			reason = args[1] if len(args) >= 2 else None
 			text = "%s was%s banned by %s." % (slack.escape(user), " also" if same_user else "", slack.escape(mod))
-			if reason is not None:
+			if reason:
 				text += " Reason: %s" % slack.escape(reason)
 		elif action == 'unban':
 			user = args[0]
@@ -102,6 +122,14 @@ class ModeratorActions:
 			text = "%s has enabled follower-only mode: minimum age %s." % (slack.escape(mod), slack.escape(time.nice_duration(duration, 0)))
 		elif action == 'followersoff':
 			text = "%s has disabled follower-only mode." % (slack.escape(mod), )
+		elif action == 'emoteonly':
+			text = "%s has enabled emote-only mode." % (slack.escape(mod), )
+		elif action == 'emoteonlyoff':
+			text = "%s has disabled emote-only mode." % (slack.escape(mod), )
+		elif action == 'subscribers':
+			text = "%s has enabled subscriber-only mode." % (slack.escape(mod), )
+		elif action == 'subscribersoff':
+			text = "%s has disabled subscriber-only mode." % (slack.escape(mod), )
 
 		elif action == 'host':
 			target = args[0]
@@ -119,9 +147,67 @@ class ModeratorActions:
 			text = "%s has cleared %s's recent-cheer notice." % (slack.escape(mod), slack.escape(cheerer))
 
 		else:
-			text = "%s did a %s: %s" % (slack.escape(mod), slack.escape(action), slack.escape(repr(args)))
+			if args:
+				text = "%s did a %s: %s" % (slack.escape(mod), slack.escape(action), slack.escape(repr(args)))
+			else:
+				text = "%s did a %s." % (slack.escape(mod), slack.escape(action))
 
-		asyncio.ensure_future(slack.send_message(text, attachments=attachments), loop=self.loop).add_done_callback(utils.check_exception)
+		return text, attachments
+
+	def on_channel_terms_action(self, message):
+		user = message['data']['requester_login']
+		term = message['data']['text']
+
+		if message['data']['type'] == 'add_permitted_term':
+			text = f"{slack.escape(user)} added '{slack.escape(term)}' as a permitted term"
+		elif message['data']['type'] == 'delete_permitted_term':
+			text = f"{slack.escape(user)} removed '{slack.escape(term)}' as a permitted term"
+		elif message['data']['type'] == 'add_blocked_term':
+			text = f"{slack.escape(user)} added {slack.escape(term)}' as a blocked term"
+		elif message['data']['type'] == 'delete_blocked_term':
+			text = f"{slack.escape(user)} removed '{slack.escape(term)}' as a blocked term"
+		else:
+			text = f"{slack.escape(user)} did a {slack.escape(message['data']['type'])} to '{slack.escape(term)}'"
+
+		if expires_at := message['data'].get('expires_at'):
+			expires_at = dateutil.parser.parse(expires_at).astimezone(config['timezone'])
+			now = datetime.datetime.now(tz=config['timezone'])
+			text += f" until {expires_at:%Y-%m-%d %H:%M:%S} ({slack.escape(time.nice_duration(expires_at - now))} from now)."
+		else:
+			text += "."
+
+		return text, []
+
+	def on_role_action(self, message):
+		user = message['data']['created_by']
+		target = message['data']['target_user_login']
+
+		if message['type'] == 'moderator_added':
+			text = f"{slack.escape(user)} added {slack.escape(target)} as a moderator."
+		elif message['type'] == 'moderator_removed':
+			text = f"{slack.escape(user)} removed {slack.escape(target)} as a moderator."
+		elif message['type'] == 'vip_added':
+			text = f"{slack.escape(user)} added {slack.escape(target)} as a VIP."
+		else:
+			text = f"{slack.escape(user)} did a {slack.escape(message['type'])} to {slack.escape(target)}."
+
+		return text, []
+
+	def on_unban_request_action(self, message):
+		mod = message['data']['created_by_login']
+		user = message['data']['target_user_login']
+
+		if message['type'] == 'approve_unban_request':
+			text = f"{slack.escape(mod)} approved {slack.escape(user)}'s unban request."
+		elif message['type'] == 'deny_unban_request':
+			text = f"{slack.escape(mod)} denied {slack.escape(user)}'s unban request."
+		else:
+			text = f"{slack.escape(mod)} did a {slack.escape(message['type'])} to {slack.escape(user)}'s unban request."
+
+		if moderator_message := message['data'].get('moderator_message'):
+			text += f" Message: {slack.escape(moderator_message)}"
+
+		return text, []
 
 	def get_chat_log(self, user):
 		attachments = []
