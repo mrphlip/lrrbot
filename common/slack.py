c3404559518075989c251f82ca00b8c13701e19d
Andreas Ots <qrpth@qrpth.eu>
2024-05-08 16:19:16 +0300

    Attempt to determine the show from the stream title if it's not set
    Also resets show, show override, and game override when the stream goes
    offline.
    
    Closes #1518.
    Closes #1519.

--- common/slack.py
+++ common/slack.py
@@ -1,7 +1,10 @@
 import json
+import logging
 
 from common.config import config
 from common import http
+
+log = logging.getLogger(__name__)
 
 async def send_message(text, **keys):
 	keys['text'] = text
@@ -12,6 +15,8 @@ async def send_message(text, **keys):
 
 	if config['slack_webhook_url'] is not None:
 		await http.request(config['slack_webhook_url'], method="POST", data=json.dumps(keys), headers=headers)
+	else:
+		log.info("Not sending a message to Slack: %r", keys)
 
 def escape(text):
 	return text \
